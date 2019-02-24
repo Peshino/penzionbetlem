577d152e91cc02889cf69d6453d50690dfbc9309
Jiri Pesek <pesek.jirka@centrum.cz>
2019-02-24 22:51:18 +0100

    Initial changes

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">
+        <p>
+            © 2013-{{ now()->year }} | všechna práva vyhrazena -
+            <a href="http://penzionbetlem.cz">PENZION BETLÉM</a>
+            | web vytvořil <a href="mailto:pesek.jirka@centrum.cz">Ing. Jiří Pešek</a>
+        </p>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
